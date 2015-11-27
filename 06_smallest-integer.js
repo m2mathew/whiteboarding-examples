179048331c4ba5199bdef9d8f3104fee6a76e6ee
Mike Mathew <m2mathew@me.com>
2015-11-27 14:35:22 -0600

    06 finished

--- 06_smallest-integer.js
+++ 06_smallest-integer.js
@@ -5,4 +5,33 @@
 
  'use strict';
 
+function smallestNumber (a,b,c) {
 
+	var myMin = a;
+
+	if(typeof(a) !== 'number' || typeof(b) !== 'number' || typeof(c) !== 'number') {
+		console.log('Please enter three numbers');
+		return 'Please enter three numbers';
+	}
+
+	if(b < myMin) {
+		myMin = b;
+	}
+
+	if(c < myMin) {
+		myMin = c;
+	}
+
+	console.log(myMin);
+	return myMin;
+}
+
+smallestNumber(1,3,5);  // 1
+
+smallestNumber(5,13,3);  // 3
+
+smallestNumber(1,'a',100);  // Please enter three numbers
+
+smallestNumber(['a','b','c'],2,19);  // Please enter three numbers
+
+smallestNumber(19,2.5,37);  // 2.5
