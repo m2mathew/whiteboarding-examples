1ec8c8e2f1ca00e978afa73593076431c05c6b03
Mike Mathew <m2mathew@me.com>
2015-11-27 15:36:06 -0600

    finshed 11

--- 11_number-positive.js
+++ 11_number-positive.js
@@ -4,3 +4,24 @@
  */
 
 'use strict';
+
+function positiveInt(array) {
+
+	 var count = 0;
+
+	 for(var i = 0; i < array.length; i++) {
+	 	if(typeof(array[i]) === 'number' && array[i] > 0) {
+	 		count++;
+	 	}
+	 }
+	 console.log(count);
+	 return count;
+}
+
+positiveInt([-1,-2,-4,1]);  // 1
+
+positiveInt([2,4,78,34,6789]);  // 5
+
+positiveInt(['beanie babies', -1, false, 'earthquake', [1,2,3], {a:1, 1:'a'}]);  // 0
+
+positiveInt([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19]);  // 19
