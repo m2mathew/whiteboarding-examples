7af23d430fbdf086230627c035e73ee9cfee9f75
Mike Mathew <m2mathew@me.com>
2015-11-27 15:27:13 -0600

    finshed 10

--- 10_sum-of-array.js
+++ 10_sum-of-array.js
@@ -5,3 +5,24 @@
 
 'use strict';
 
+function sumOfArray(array) {
+
+	var sum = 0;
+
+	for(var i = 0; i < array.length; i++) {
+		if(typeof(array[i]) === 'number') {
+			sum = sum + array[i];
+		}
+	}
+
+	console.log(sum);
+	return sum;
+}
+
+sumOfArray([1,2,3,4]);  // 10
+
+sumOfArray([11,89,1]);  // 101
+
+sumOfArray([42,42,42]);  // 126
+
+sumOfArray(['a','kitten','samurai',19]);  // 19
