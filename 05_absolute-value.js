152405d1dd01aef8b6c5eaa7568a4776c8ee56db
Mike Mathew <m2mathew@me.com>
2015-11-27 01:13:32 -0600

    finished 05

--- 05_absolute-value.js
+++ 05_absolute-value.js
@@ -5,4 +5,31 @@
 
  'use strict';
 
+function absVal(number) {
 
+	if(typeof(number) !== 'number') {
+		console.log('Please enter a number');
+		return 'Please enter a number';
+	}
+	else if(number < 0) {
+		console.log(number * -1);
+		return number * -1;
+	}
+	else {
+		console.log(number);
+		return number;
+	}
+}
+
+
+absVal(19);  // 19
+
+absVal(-19);  // 19
+
+absVal('ewok');  // Please enter a number
+
+absVal('20.02');  // Please enter a number
+
+absVal(20.02);  // 20.02
+
+absVal(0);  // 0
