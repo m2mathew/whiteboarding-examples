969839884a657712eafdb661efffdee01abe9195
Mike Mathew <m2mathew@me.com>
2015-11-27 23:46:52 -0600

    finished 14

--- 14_number-of-each-letter.js
+++ 14_number-of-each-letter.js
@@ -5,3 +5,29 @@
  */
 
 'use strict';
+
+function letterCount(string) {
+
+	var myObj = {};
+
+	for(var i = 0; i < string.length; i++) {
+
+		if(myObj.hasOwnProperty(string.charAt(i)) !== true) {
+			myObj[string.charAt(i)] = 1;
+		}
+		else if(myObj.hasOwnProperty(string.charAt(i)) === true) {
+			myObj[string.charAt(i)]++;
+		}
+	}
+
+	console.log(myObj);
+	return myObj;
+}
+
+letterCount('pandas');  // {a:2, p:1, n:1, d:1, s:1}
+
+letterCount('bob');  // {b:2, 0:1}
+
+letterCount('banana');  // {a:3, b:1, n:2}
+
+letterCount('aaron');  // {a:2, r:1, o:1, n:1}
