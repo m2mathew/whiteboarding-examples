af578e17ad665b291f3faa0db27dda5a72632d19
Mike Mathew <m2mathew@me.com>
2015-11-27 21:56:56 -0600

    finished 13

--- 13_has-own-property.js
+++ 13_has-own-property.js
@@ -7,3 +7,36 @@
  */
 
 'use strict';
+
+function ownProperty(myObject, myString) {
+
+	for(var prop in myObject) {
+		if(prop === myString) {
+			console.log('true');
+			return true;
+		}
+	}
+	console.log('false');
+	return false;
+}
+
+ownProperty(
+	{
+		name: 'Mike',
+		car: 'Audi',
+		kids: 3
+	}, 'name');  // true
+
+ownProperty(
+	{
+		name: 'Chewie',
+		animal: 'dog',
+		numLegs: 4
+	}, 'hasTail');  // false
+
+ownProperty(
+	{
+		title: 'Lord of the Rings',
+		author: 'J.R.R. Tolkien',
+		volumes: 3
+	}, 'volumes');  // true
